c3bf67a8f01876a5657eb73a2cc792b9017c78f1
Sahil Nikshe <nikshesahil@gmail.com>
2024-04-28 18:38:51 +0530

    minor changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
 import "./App.css";
 import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
 
 function App() {
-  return (
-    <>
-      <h1 className="text-3xl font-bold underline">Hello world!</h1>
-      <Button>Click me</Button>
-    </>
-  );
+  return <></>;
 }
 
 export default App;
